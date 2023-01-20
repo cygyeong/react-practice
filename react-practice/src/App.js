5b9000872d3db69edfce40fdb11971b4a543fe69
SSAFY <cyellowgreen@gmail.com>
2023-01-20 11:42:26 +0900

    capture practice

---
+++ react-practice/src/App.js
@@ -0,0 +1,10 @@
+import OnHtmlToPng from './components/capture/capture.component'
+function App() {
+  return (
+    <div className="App">
+      <OnHtmlToPng/>
+    </div>
+  );
+}
+
+export default App;
